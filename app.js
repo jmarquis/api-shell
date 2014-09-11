06d9bb51ca0336f061546d0c8f9d4814f30052d5
Jeremy Marquis <jeremy@jeremymarquis.com>
2014-09-11 16:50:03 -0400

    prospective groundwork

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-var express = require("express"),
-	config = require("./config.json");
+var config = require("./config.json"),
+	express = require("express"),
+	http = require("http"),
+	io = require("socket.io");
 
-var Application = function (config, server) {
+var Application = function (config) {
 
 	this.config = config;
-	this.server = server;
-	this.listener = this.server.listen(config.port, function () {
-		console.log("Server listening on port %d", this.server.address().port);
-	});
+	this.express = express();
+	this.server = http.Server(this.express);
+	this.io = io(this.server);
+	this.listener = null;
+
+	this.run = function () {
+		var listener = this.listener = this.server.listen(this.config.port, function () {
+			console.log("Server listening on port %d", listener.address().port);
+		});
+	};
 
 	return this;
 
 };
 
-var app = new Application(config, express());
+var app = new Application(config);
+
+app.io.on("connection", function (socket) {
+
+	console.log("new connection");
+
+});
+
+app.run();
